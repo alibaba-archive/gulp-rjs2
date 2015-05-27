270d81357ec77ea88283241af0aad6f3bb3a3161
Sune Bear <hi@sunebear.com>
2015-05-27 18:14:12 +0800

    Add path support

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
-/*
+
+/**
  * https://github.com/teambition/gulp-rjs2
- *
  * Licensed under the MIT license.
  */
 
+var fs = require('fs');
 var through = require('through2');
 var gutil = require('gulp-util');
 var requirejs  = require('requirejs');
@@ -15,28 +16,19 @@ var PluginError = gutil.PluginError;
 module.exports = function rjs(options) {
   var stream = through.obj();
   var path = options.out;
-  var modulesName = null;
-  if (options.componentModules && options.componentNamespace) {
-    modulesName = options.componentModules.map(function (name) {
-      return name.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-    }).join('|');
-  }
+
+  var comName = options.name;
+  var comNamespace = options.componentNamespace;
+  var excludeModules = [];
 
   options.out = function(string) {
-    if (modulesName) {
-      string = string
-        .replace(new RegExp('define\\([\'\"](' + modulesName + ')', 'g'), function(match, p1) {
-          return match.replace(p1, p1 === options.name ? options.componentNamespace : (options.componentNamespace + '.' + p1));
-        })
-        .replace(/define\([\w\W]*?\[([\w\W]*?)\]/g, function(match, p1) {
-          return match.replace(p1, p1.replace(new RegExp('(' + modulesName + ')', 'g'), options.componentNamespace + '.$1'));
-        })
-        .replace(new RegExp("require\\('(" + modulesName + ")'\\)", 'g'), function(match, p1) {
-          return match.replace(p1, options.componentNamespace + '.' + p1);
-        })
-        .replace(new RegExp('require\\("(' + modulesName + ')"\\)', 'g'), function(match, p1) {
-          return match.replace(p1, options.componentNamespace + '.' + p1);
-        });
+    if (comNamespace && options.exclude) {
+      options.exclude.forEach(function(ele){
+        var filePath = options.baseUrl + ele + '.js';
+        var file = fs.readFileSync(filePath, 'utf8');
+        excludeModules = excludeModules.concat(matchFirstArray(file));
+      });
+      string = addNameSpaceForUMD(string);
     }
     stream.push(new gutil.File({
       path: path,
@@ -51,5 +43,40 @@ module.exports = function rjs(options) {
     stream.emit('error', err);
   });
 
+  function addNameSpaceForUMD(string) {
+    return string
+    .replace(/define\([\'\"](.*?)[\'\"]/g, function(match, p1) {
+      // AMD Define
+      if (!~excludeModules.indexOf(p1)) {
+        return match.replace(p1, p1 === comName ? comNamespace : (comNamespace + '.' + p1));
+      }
+    })
+    .replace(/define\([\w\W]*?\[([\w\W]*?)\]/g, function(match, p1) {
+      // AMD Deps
+      var excludeModulesWithAMD = excludeModules.concat(['require','exports','module']);
+      return match.replace(p1, p1.replace(/[\'\"](.*?)[\'\"]/g, function(match, p1){
+        return match.replace(p1, !~excludeModulesWithAMD.indexOf(p1) ? (comNamespace + '.' + p1) : p1);
+      }));
+    })
+    .replace(/require\([\'\"](.*?)[\'\"]\)/g, function(match, p1) {
+      // CMD Require
+      if (!~excludeModules.indexOf(p1)) {
+        return match.replace(p1, comNamespace + '.' + p1);
+      }
+    });
+  }
+
+  function matchFirstArray(file) {
+    var reg = /\[(.*)\]/;
+    var matchValue = file.match(reg);
+    if (matchValue) {
+      // Convert string to array
+      matchValue = JSON.parse(matchValue[0].replace(/\'/g, '"'));
+    } else {
+      matchValue = [];
+    }
+    return matchValue;
+  }
+
   return stream;
-}
+};
